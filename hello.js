e879fc4cf7e7eaed56c55379cb61070b8a88451c
simyeen <simyeen@naver.com>
2021-10-01 01:43:11 +0900

    bulit-in obj

---
+++ hello.js
@@ -0,0 +1,24 @@
+window.onload = function () {
+  "use strict";
+
+  var a = "123456789";
+  console.log(a.indexOf(0));
+
+  String;
+  var s = String;
+  console.log(s.prototype);
+
+  var st = String(123);
+
+  var obj = new String(123);
+  console.log(typeof obj, typeof s, typeof st);
+
+  debugger;
+
+  var node = document.querySelector("div");
+
+  debugger;
+
+  show();
+  debugger;
+};
---
+++ test.js
@@ -0,0 +1,18 @@
+var obj = Object;
+var obj = {};
+
+console.log(Object.hasOwnProperty("length"));
+
+var point = { book: "책" };
+console.log(point.toString());
+
+var o = new Object();
+
+console.log(parseInt("12px"));
+
+console.log(typeof NaN);
+
+console.log(NaN, NaN === NaN);
+
+var uri = "data?one=번&two=호";
+console.log(encodeURI(uri));
