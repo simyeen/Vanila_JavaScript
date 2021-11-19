6f23969a8668d75047c35e8cb85936083265c111
simyeen <simyeen@naver.com>
2021-11-19 17:05:31 +0900

    bible

--- bible/Generator.js
+++ bible/Generator.js
@@ -2,6 +2,8 @@ function* sports(one) {
   yield one + 10;
   yield;
   const value = yield one + 50;
+  const value2 = yield one + 50;
+  console.log("ㅎㅇ", value, value2);
 }
 
 const obj = sports(30);
@@ -9,3 +11,4 @@ console.log(obj.next());
 console.log(obj.next());
 console.log(obj.next());
 console.log(obj.next(200));
+console.log(obj.next(201));
